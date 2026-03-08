2c5bdb4741a5fcc8cbba28e95a53e5ec7d80a318
Alper Elmaser <alperelmaser67@hotmail.com>
2026-03-08 18:02:06 +0300

    uploadthing add button fixed v7

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
 
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)',"/api/uploadthing(.*)","/blog(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   
